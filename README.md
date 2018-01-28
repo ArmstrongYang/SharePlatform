737940c99432dde382f1fc6d0a4bbfe49024ae1a
ArmstrongYang <913622677@qq.com>
2018-01-28 21:29:54 +0800

    add main.py and task.ipynb

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # SharePlatform
 The Platform of Machine and Deep Learning.
+
+# Jupyter Notebook
+```
+jupyter notebook
+```
+
+# TensorBoard
+```
+tensorboard --logdir =temp/tflog
+```
+
+# Mnist
---
+++ main.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+##  coding: utf-8 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+def main():
+    pass
+    
+if __name__ == '__main__':
+    print(__file__)
+    main()
+    print('end')
+    exit()
